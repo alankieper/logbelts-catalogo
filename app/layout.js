fbe39f7ff793c975b9b5df3318a8ff2d4021e2e0
alankieper <alankieper@gmail.com>
2026-08-25 12:56:20 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Logbelts - Mejoras de Catálogo',
+  description: 'Aplicación interna de mejoras de catálogo',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
